5753d55b3f1f29994656b511fa0a516257e9d205
Adrien <148849224+AB-Ynov@users.noreply.github.com>
2024-01-03 12:33:27 +0100

    Delete src directory

--- src/views/paged-inscription.js
+++
@@ -1,37 +0,0 @@
-import React from 'react'
-
-import { Helmet } from 'react-helmet'
-
-import './paged-inscription.css'
-
-const PagedInscription = (props) => {
-  return (
-    <div className="paged-inscription-container">
-      <Helmet>
-        <title>exported project</title>
-      </Helmet>
-      <div className="paged-inscription-paged-inscription">
-        <span className="paged-inscription-text">
-          <span>Amby - Inscription</span>
-        </span>
-        <span className="paged-inscription-text02">
-          <span>
-            Nom complet :  Adresse e-mail :  Nom d&apos;utilisateur :  Mot de
-            passe : 
-          </span>
-        </span>
-        <span className="paged-inscription-text04">
-          <span>Amby - Inscription</span>
-        </span>
-        <span className="paged-inscription-text06">
-          <span>Déjà inscrit ?  Se connecter ici</span>
-        </span>
-        <span className="paged-inscription-text08">
-          <span>S&apos;inscrire</span>
-        </span>
-      </div>
-    </div>
-  )
-}
-
-export default PagedInscription
